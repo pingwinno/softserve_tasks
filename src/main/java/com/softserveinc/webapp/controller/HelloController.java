d4dafecc54e3c000db403e94241da67eacb46d0f
mromanc <pingwinno@gmail.com>
2020-04-09 22:08:04 +0300

    M1T3: Add index page.

--- src/main/java/com/softserveinc/webapp/controller/HelloController.java
+++ src/main/java/com/softserveinc/webapp/controller/HelloController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
     @GetMapping
-    public String doHelloWorld(){
-        return " Hello World";
+    public String doHelloWorld() {
+        return " <!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<body>" +
+                "<p> Hello, %username%. </p>" +
+                "<p> This is CRUD API for managing user entities.</p> " +
+                "<p> Documentation can be found " +
+                " <a href=/static/docs/index.html>here</a></p> " +
+                "</body>\n" +
+                "</html>\n";
     }
 }
